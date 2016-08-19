2b704233c34054b51756f82b79ef646f9e557e3c
Damiaan Dufaux <info@devian.tk>
2016-08-19 22:00:13 +0200

    Initial Commit

---
+++ aStar/aStar.h
@@ -0,0 +1,19 @@
+//
+//  aStar.h
+//  aStar
+//
+//  Created by Damiaan Dufaux on 19/08/16.
+//  Copyright © 2016 Damiaan Dufaux. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for aStar.
+FOUNDATION_EXPORT double aStarVersionNumber;
+
+//! Project version string for aStar.
+FOUNDATION_EXPORT const unsigned char aStarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aStar/PublicHeader.h>
+
+
