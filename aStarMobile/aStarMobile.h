dba56572a99fde8e006b8ded70a4bc3083a1a92a
Damiaan Dufaux <damiaan@dpro.be>
2017-01-11 22:01:12 +0100

    Add mobile framework

---
+++ aStarMobile/aStarMobile.h
@@ -0,0 +1,19 @@
+//
+//  aStarMobile.h
+//  aStarMobile
+//
+//  Created by Damiaan on 11/01/17.
+//  Copyright © 2017 Damiaan Dufaux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for aStarMobile.
+FOUNDATION_EXPORT double aStarMobileVersionNumber;
+
+//! Project version string for aStarMobile.
+FOUNDATION_EXPORT const unsigned char aStarMobileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aStarMobile/PublicHeader.h>
+
+
